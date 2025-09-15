a450fa11930c0ed0a93d457cc07fa36043ded930
Nath <nathalia.n.reis@aluno.senai.br>
2025-09-15 10:44:36 -0300

    feat: cria o arquivo para selects

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -1,17 +1,3 @@
-CREATE TABLE artistas {
-    id SERIAL PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    tipo VARCHAR(50) NOT NULL,
-    membros_quantidade INT,
-    genero_musical VARCHAR(50),
-    pais_origem VARCHAR(30),
-    ano_inicio INT,
-    musica_mais_ouvida VARCHAR(100),
-    ativo BOOLEAN DEFAULT TRUE
-};
+SELECT COUNT(*) AS total_artistas FROM artistas;
 
-INSERT INTO artistas (nome, tipo, membros_quantidade, genero_musical, pais_origem, ano_inicio, musica_mais_ouvida, ativp)
-VALUES 
-(
-    'NewJeans', 'Grupo', 5, 'Kpop, Nostalgic Y2K', 'Coreia, Vietnã e Austrália', 2022, 'Ditto', true
-);
+SELECT *FROM artistas
---
+++ queries/02-select.sql
@@ -0,0 +1,29 @@
+CREATE TABLE artistas {
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    tipo VARCHAR(50) NOT NULL,
+    membros_quantidade INT,
+    genero_musical VARCHAR(50),
+    pais_origem VARCHAR(30),
+    ano_inicio INT,
+    musica_mais_ouvida VARCHAR(100),
+    ativo BOOLEAN DEFAULT TRUE
+};
+
+INSERT INTO artistas (nome, tipo, membros_quantidade, genero_musical, pais_origem, ano_inicio, musica_mais_ouvida, ativp)
+VALUES 
+(
+    'NewJeans', 'Grupo', 5, 'Kpop, Nostalgic Y2K', 'Coreia, Vietnã e Austrália', 2022, 'Ditto', true
+);
+
+INSERT INTO artistas (nome, tipo, membros_quantidade, genero_musical, pais_origem, ano_inicio, musica_mais_ouvida, ativp)
+VALUES 
+(
+    'Itzy', 'Grupo', 5, 'Kpop', 'Coreia', 2019, 'Wannabe', true
+);
+
+INSERT INTO artistas (nome, tipo, membros_quantidade, genero_musical, pais_origem, ano_inicio, musica_mais_ouvida, ativp)
+VALUES 
+(
+    'Xdinary Heroes', 'Banda', 6, 'Rock', 'Coreia', 2021, 'Happy Death Day', true
+);
